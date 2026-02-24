fd2c021a3a995fde8e61a61e8b4716a54396c7cc
kevickstrom <ore.sailor@gmail.com>
2026-02-24 12:31:42 -0800

    adding custom ping protocol interpreter

--- src/ping_task.cpp
+++ src/ping_task.cpp
@@ -9,10 +9,17 @@
 // https://docs.bluerobotics.com/ping-protocol/pingmessage-common/
 // https://docs.bluerobotics.com/ping-protocol/pingmessage-ping1d/
 
+static QueueHandle_t ping_queue;
+
+QueueHandle_t get_ping_queue()
+{
+    return ping_queue;
+}
+
 static void ping_task(void *arg)
 {
     uart_transaction_t trans;
-    ping_data_t ping;
+    ping_distance_t distance_response;
 
     while (1)
     {
@@ -22,8 +29,9 @@ static void ping_task(void *arg)
 
         memset(&trans, 0, sizeof(trans));
         trans.device = PING;
-        memcpy(trans.tx_buf, cmd, strlen(cmd));
-        trans.tx_len = strlen(cmd);
+
+        trans.tx_len = ping_distance(trans.tx_buf, sizeof(trans.tx_buf));
+
         trans.timeout_ms = 200;
         trans.caller = xTaskGetCurrentTaskHandle();
 
@@ -31,11 +39,13 @@ static void ping_task(void *arg)
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
         // TODO: Parse Ping Response with funcs below
+        parse_distance(trans.rx_buf, trans.rx_len, &distance_response);
 
         // publish to ping queue?
+        xQueueSend(get_ping_queue(), &distance_response, 0);
 
         // send to SD task
-        xQueueSend(get_record_queue(), &ping, 0);
+        xQueueSend(get_record_queue(), &distance_response, 0);
 
         vTaskDelay(pdMS_TO_TICKS(g_sample_interval_ms));
     }
@@ -107,7 +117,7 @@ static size_t build_ping_command(uint8_t *buf, size_t bufsize, uint16_t msg_id, 
 
 /* PING COMMANDS */
 // 1212 Distance
-static size_t distance(uint8_t *buf, size_t size)
+static size_t ping_distance(uint8_t *buf, size_t size)
 {
     uint8_t payload[2];
     write_u16_le(payload, 1212); // distance request
@@ -115,15 +125,52 @@ static size_t distance(uint8_t *buf, size_t size)
     return build_ping_command(buf, size, 1212, payload, sizeof(payload));
 }
 
-// TODO: parse the full response and grab the buf, buf len
 
-
-
-// these will parse the payload as the input buf
+// these will parse the uart_transaction_t payload as their input buf and output to their corresponding struct
 /* PARSING RECEIVED DATA*/
 
-static bool parse_distance(const uint8_t *buf, size_t len, ping_data_t *ping)
+static bool parse_distance(const uint8_t *buf, size_t len, ping_distance_t *distance_response)
 {
     if (len < 24) return false;
-    uf (buf[0])
+    if (buf[0] != 0x42 || buf[1] != 0x52) return false; // check start bytes
+
+    uint16_t payload_len = buf[2] | (buf[3] << 8);
+    uint16_t msg_id = buf[4] | (buf[5] << 8);
+
+    if (msg_id != 1212) return false; // check message ID
+
+    size_t expected_len = 1+1+2+2+1+1+payload_len+2;
+    if (len < expected_len) return false; // check total length
+
+    // check checksum
+    uint16_t recv_checksum = buf[expected_len - 2] | (buf[expected_len - 1] << 8);
+    uint16_t calc_checksum = compute_checksum(buf, expected_len - 2);
+    if (recv_checksum != calc_checksum) return false;
+ 
+    // Ping distance format:
+    // u32 distance_mm
+    // u16 confidence
+    // u16 transmit_duration
+    // u32 ping_number
+    // u32 scan_start
+    // u32 scan_length
+    // u32 gain_setting
+    uint32_t distance_mm = buf[8] | (buf[9] << 8) | (buf[10] << 16) | (buf[11] << 24);
+    uint16_t confidence = buf[12] | (buf[13] << 8   );
+    uint16_t transmit_duration = buf[14] | (buf[15] << 8);
+    uint32_t ping_number = buf[16] | (buf[17] << 8) | (buf[18] << 16) | (buf[19] << 24);
+    uint32_t scan_start = buf[20] | (buf[21] << 8) | (buf[22] << 16) | (buf[23] << 24);
+    uint32_t scan_length = buf[24] | (buf[25] << 8) | (buf[26] << 16) | (buf[27] << 24);
+    uint32_t gain_setting = buf[28] | (buf[29] << 8) | (buf[30] << 16) | (buf[31] << 24);
+
+    distance_response->distance_mm = distance_mm;
+    distance_response->confidence = confidence;
+    distance_response->transmit_duration_us = transmit_duration;
+    distance_response->ping_number = ping_number;
+    distance_response->scan_start_mm = scan_start;
+    distance_response->scan_length_mm = scan_length;
+    distance_response->gain_setting = gain_setting;
+    distance_response->timestamp = xTaskGetTickCount();
+
+    return true;
 }
--- src/ping_task.h
+++ src/ping_task.h
@@ -3,11 +3,24 @@
 #include "freertos/queue.h"
 #include <stdint.h>
 
+// Ping distance format:
+    // u32 distance_mm
+    // u16 confidence  %
+    // u16 transmit_duration  us
+    // u32 ping_number
+    // u32 scan_start  mm
+    // u32 scan_length  mm 
+    // u32 gain_setting
 typedef struct {
-    float depth_m;
-    uint8_t confidence;
+    uint32_t distance_mm;
+    uint16_t confidence;
+    uint16_t transmit_duration_us;
+    uint32_t ping_number;
+    uint32_t scan_start_mm;
+    uint32_t scan_length_mm;
+    uint32_t gain_setting;
     uint32_t timestamp;
-} ping_data_t;
+} ping_distance_t;
 
 QueueHandle_t get_ping_queue();
 void init_ping_task();
